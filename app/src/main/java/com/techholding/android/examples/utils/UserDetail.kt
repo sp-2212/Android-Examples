a3d949973a1c7f0c13cda267b53c74ae36b2270c
sp-2212 <sarthak.patel@techholding.co>
2023-02-10 00:16:19 +0530

    (feat)[Partial] : Added Fragment

---
+++ app/src/main/java/com/techholding/android/examples/utils/UserDetail.kt
@@ -0,0 +1,9 @@
+package com.techholding.android.examples.utils
+
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class UserDetail {
+
+    var userID = Firebase.auth
+}
