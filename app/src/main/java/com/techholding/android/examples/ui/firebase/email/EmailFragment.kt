429d0ebff8671326f0541888b20903d83206dbb5
sp-2212 <sarthak.patel@techholding.co>
2023-02-10 11:29:58 +0530

    (feat)[Partial] : Email XML Fragment Completed

--- app/src/main/java/com/techholding/android/examples/ui/firebase/email/EmailFragment.kt
+++ app/src/main/java/com/techholding/android/examples/ui/firebase/email/EmailFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.techholding.android.examples.R
+import com.techholding.android.examples.databinding.FragmentEmailAuthBinding
 
 class EmailFragment : Fragment() {
 
+    private var _binding: FragmentEmailAuthBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_email_auth, container, false)
+        _binding = FragmentEmailAuthBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
     }
 }
