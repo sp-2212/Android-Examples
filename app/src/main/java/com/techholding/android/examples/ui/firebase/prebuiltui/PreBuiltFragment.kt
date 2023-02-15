b7550c9feb04b6c0df9c909ab04db4de3c126b9e
sp-2212 <sarthak.patel@techholding.co>
2023-02-15 15:33:38 +0530

    (feat)! : Pre-Built UI Done

--- app/src/main/java/com/techholding/android/examples/ui/firebase/prebuiltui/PreBuiltFragment.kt
+++ app/src/main/java/com/techholding/android/examples/ui/firebase/prebuiltui/PreBuiltFragment.kt
@@ -1,5 +1,6 @@
 package com.techholding.android.examples.ui.firebase.prebuiltui
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.techholding.android.examples.R
 import com.techholding.android.examples.databinding.FragmentPreBuiltAuthBinding
 import com.techholding.android.examples.utils.UserDetail
 
@@ -15,6 +20,12 @@ class PreBuiltFragment : Fragment() {
 
     private var _binding: FragmentPreBuiltAuthBinding? = null
     private val binding get() = _binding!!
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -48,7 +59,27 @@ class PreBuiltFragment : Fragment() {
 
     private fun initSignInButton() {
         binding.signIn.setOnClickListener {
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.PhoneBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.FacebookBuilder().build())
 
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.ic_launcher_foreground)
+                .build()
+            signInLauncher.launch(signInIntent)
+        }
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+            UserDetail.authState.value = true
+        } else {
+            UserDetail.authState.value = false
+            Toast.makeText(context, "Sign-In Failed", Toast.LENGTH_SHORT).show()
         }
     }
 
