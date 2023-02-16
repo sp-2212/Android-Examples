a0eba44ae6584ddd4195bd2690ac57f5d7a0f891
jinilpatel3108 <jinil.patel@techholding.co>
2023-02-16 18:14:43 +0530

    (feat)! Aws initial auth

--- app/src/main/java/com/techholding/android/examples/Application.kt
+++ app/src/main/java/com/techholding/android/examples/Application.kt
@@ -1,5 +1,8 @@
 package com.techholding.android.examples
 
+import android.util.Log
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.core.Amplify
 import com.facebook.FacebookSdk
 import com.facebook.appevents.AppEventsLogger
 import com.google.firebase.FirebaseApp
@@ -7,6 +10,12 @@ import com.google.firebase.FirebaseApp
 class Application : android.app.Application(){
     override fun onCreate() {
         super.onCreate()
+        try {
+            Amplify.configure(applicationContext)
+            Log.i("MyAmplifyApp", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
+        }
 
         FirebaseApp.initializeApp(applicationContext)
         FacebookSdk.sdkInitialize(applicationContext)
---
+++ app/src/main/java/com/techholding/android/examples/utils/AWSAuthManager.kt
@@ -0,0 +1,9 @@
+package com.techholding.android.examples.utils
+
+import androidx.appcompat.app.AppCompatActivity
+
+object AWSAuthManager {
+
+
+
+}
